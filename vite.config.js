77eb4818633809daa8abef3fa09bcfeb35762ac3
jose alfaro <alfaro.ayzama.jose.fernando@gmail.com>
2024-06-30 12:12:31 -0400

    pruebas

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     build: {
         manifest: true,
         outDir: 'public/build',
+        manifest: true,
         rollupOptions: {
             input: '/resources/js/app.js',
         },
